de360e925d1a98dec8191065beaae698fe91d8e6
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-10-30 18:39:25 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -10,7 +10,12 @@ for i in $(find . -type f -name "values.yaml"); do
     echo "-------------------------" >> $output_file
     echo "$i" >> $output_file
     echo "-------------------------" >> $output_file
-    cat "$i" | grep 'imageTag\|image\|#renovate' | sed -e 's/^[ \t]*//' >> $output_file
+    while IFS= read -r line; do
+        if [[ $line =~ (imageTag|image|[#]renovate) ]]; then
+            echo "$line" | sed -e 's/^[ \t]*//' >> "$OUTPUT_FILE"
+        fi
+    done < "$i"
+    echo -e "\n" >> "$OUTPUT_FILE"
 done
 if ! git diff --quiet -- "$output_file"; then
     # Add, commit, and push the file to the GitHub repository
