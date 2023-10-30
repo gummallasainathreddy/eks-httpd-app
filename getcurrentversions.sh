e5452507cf088b1d24b4ed90645daa8f4d8642d8
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-10-30 18:08:29 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -12,8 +12,13 @@ for i in $(find . -type f -name "values.yaml"); do
     echo "-------------------------" >> $output_file
     cat "$i" | grep 'imageTag\|#renovate' | sed -e 's/^[ \t]*//' >> $output_file
 done
-git add "$output_file"
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
-git commit -m "Added output.md via GitHub Action"
-git push
+if [[ -n $(git status --porcelain) ]]; then
+    # Add, commit, and push the file to the GitHub repository
+    git add "$output_file"
+    git config --global user.email "github-actions[bot]@users.noreply.github.com"
+    git config --global user.name "github-actions[bot]"
+    git commit -m "Added output.md via GitHub Action"
+    git push
+else
+    echo "No changes to commit."
+fi
