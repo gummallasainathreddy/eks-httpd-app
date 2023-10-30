d5a8022d670cd69d7e3c1c9853cd878316357d77
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-10-30 17:31:54 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Define the output file name
+output_file="output.md"
+
+# Clear the contents of the output file
+> $output_file
+
 for i in $(find . -type f -name "values.yaml"); do
-    echo "-------------------------"
-    echo "$i"
-    echo "-------------------------"
-    cat "$i" | grep 'imageTag\|#renovate' | sed -e 's/^[ \t]*//'
+    echo "-------------------------" >> $output_file
+    echo "$i" >> $output_file
+    echo "-------------------------" >> $output_file
+    cat "$i" | grep 'imageTag\|#renovate' | sed -e 's/^[ \t]*//' >> $output_file
 done
