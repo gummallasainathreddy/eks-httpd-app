959ef17c5ec6c4e7ab17ad7794df58380cf4a80c
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-10-31 12:18:36 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -4,14 +4,15 @@
 output_file="output.md"
 
 # Clear the contents of the output file
-> "$output_file"
+> $output_file
 
 # Create a Markdown table header
 echo "| Image | Image Tag |" >> "$output_file"
 echo "|-------|-----------|" >> "$output_file"
-
-# Loop through the values.yaml files
 for i in $(find . -type f -name "values.yaml"); do
+    echo "-------------------------" >> $output_file
+    echo "$i" >> $output_file
+    echo "-------------------------" >> $output_file
     image=""
     image_tag=""
 
@@ -26,18 +27,17 @@ for i in $(find . -type f -name "values.yaml"); do
             image_tag="${BASH_REMATCH[1]}"
         fi
     done < "$i"
-
+    #echo -e "\n" >> "$output_file"
     # Print the extracted information as a row in the table
     echo "| $image | $image_tag |" >> "$output_file"
 done
-
 if ! git diff --quiet -- "$output_file"; then
     # Add, commit, and push the file to the GitHub repository
     git add "$output_file"
     git config --global user.email "github-actions[bot]@users.noreply.github.com"
     git config --global user.name "github-actions[bot]"
-    git commit -m "Added $output_file via GitHub Action"
+    git commit -m "Added output.md via GitHub Action"
     git push
 else
     echo "No changes to commit."
-fi
+fi 
