6717ad472093a85b4fe3660de8ad3130e23d0fad
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-11-02 14:49:16 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -3,14 +3,29 @@
 # Define the output file name
 output_file="output.md"
 
-# Clear the contents of the output file
-> "$output_file"
+# Clear the contents of the output file 
+> $output_file
+echo "# Current Versions In Each Environment" >> $output_file
+for i in $(find . -type f -name "values.yaml"); do
+    #echo "-------------------------" >> $output_file
+    echo "## $i" >> $output_file
+    #echo "-------------------------" >> $output_file
+    image=""
+    image_tag=""
+    # Create a Markdown table header
+    echo "| Image | Image Tag |" >> "$output_file"
+    echo "|-------|-----------|" >> "$output_file"
 
-for i in $(find -name values.yaml); do
-    echo "------------------------------------" >> "$output_file"
-    echo "$i" >> "$output_file"
-    #echo "------------------------------------" >> "$output_file"
-    cat "$i" | grep 'image|imageTag\|#renovate' | sed -e 's/^[ \t]*//' >> "$output_file"
+    # Extract lines containing "image" and "imageTag"
+    cat "$i" | grep -E 'image:|imageTag:' | sed -e 's/^[ \t]*//' | tr ' \t' '\n' | while IFS= read -r line; do
+        if [[ $line =~ ^image:\ (.+) ]]; then
+            image="${BASH_REMATCH[1]}"
+        elif [[ $line =~ ^imageTag:\ (.+) ]]; then
+            image_tag="${BASH_REMATCH[1]}"
+        fi
+        done < "$i"
+    # Print the extracted information as a row in the table
+    echo "| $image | $image_tag |" >> "$output_file"
 done
 if ! git diff --quiet -- "$output_file"; then
     # Add, commit, and push the file to the GitHub repository
