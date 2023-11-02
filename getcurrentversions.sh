503756e24d3a56feb0290f8572f754ecb437fc6c
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-11-02 16:58:47 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -3,30 +3,38 @@
 # Define the output file name
 output_file="output.md"
 
-# Clear the contents of the output file 
-> $output_file
-echo "# Current Versions In Each Environment" >> $output_file
+# Clear the contents of the output file
+> "$output_file"
+
+# Create the table header
+echo "# Current Versions In Each Environment" >> "$output_file"
+echo "| Environment | Image | Image Tag |" >> "$output_file"
+echo "|-------------|-------|-----------|" >> "$output_file"
+
+# Loop through the values.yaml files
 for i in $(find . -type f -name "values.yaml"); do
-    #echo "-------------------------" >> $output_file
-    echo "## $i" >> $output_file
-    #echo "-------------------------" >> $output_file
+    # Extract the environment name from the directory
+    environment=$(dirname "$i" | sed 's/.\///')
+
     image=""
     image_tag=""
-    # Create a Markdown table header
-    echo "| Image | Image Tag |" >> "$output_file"
-    echo "|-------|-----------|" >> "$output_file"
 
-    # Extract lines containing "image" and "imageTag"
-    cat "$i" | grep -E 'image:|imageTag:' | sed -e 's/^[ \t]*//' | tr ' \t' '\n' | while IFS= read -r line; do
+    # Extract lines containing "image" and "imageTag" from the values.yaml file
+    while IFS= read -r line; do
         if [[ $line =~ ^image:\ (.+) ]]; then
             image="${BASH_REMATCH[1]}"
         elif [[ $line =~ ^imageTag:\ (.+) ]]; then
             image_tag="${BASH_REMATCH[1]}"
         fi
-        done < "$i"
-    # Print the extracted information as a row in the table
-    echo "| $image | $image_tag |" >> "$output_file" 
+    done < "$i"
+
+    # Check if both image and imageTag are non-empty
+    if [ -n "$image" ] && [ -n "$image_tag" ]; then
+        # Print the extracted information as a row in the table
+        echo "| $environment | $image | $image_tag |" >> "$output_file"
+    fi
 done
+
 if ! git diff --quiet -- "$output_file"; then
     # Add, commit, and push the file to the GitHub repository
     git add "$output_file"
