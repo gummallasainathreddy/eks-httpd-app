6a486539b4b8c843fce0aa6155193ae3589abc01
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-11-02 14:26:41 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -6,19 +6,11 @@ output_file="output.md"
 # Clear the contents of the output file
 > "$output_file"
 
-# Create the table header
-echo "| Image | Image Tag |" >> "$output_file"
-echo "|-------|-----------|" >> "$output_file"
-
-# Loop through the values.yaml files
-for i in $(find . -type f -name "values.yaml"); do
-    image=$(grep -o 'image:\s\+\S\+' "$i" | awk '{print $2}')
-    image_tag=$(grep -o 'imageTag:\s\+\S\+' "$i" | awk '{print $2}')
-    
-    if [ -n "$image" ] && [ -n "$image_tag" ]; then
-        echo "## $i" >> "$output_file"
-        echo "| $image | $image_tag |" >> "$output_file"
-    fi
+for i in $(find -name values.yaml); do
+    echo "------------------------------------" >> "$output_file"
+    echo "$i" >> "$output_file"
+    echo "------------------------------------" >> "$output_file"
+    cat "$i" | grep 'imageTag\|#renovate' | sed -e 's/^[ \t]*//' >> "$output_file"
 done
 if ! git diff --quiet -- "$output_file"; then
     # Add, commit, and push the file to the GitHub repository
