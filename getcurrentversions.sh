5a64340409d394956fb69ef51f8535a997a7d248
gummallasainathreddy <106722380+gummallasainathreddy@users.noreply.github.com>
2023-11-03 19:43:34 +0530

    Update getcurrentversions.sh

--- getcurrentversions.sh
+++ getcurrentversions.sh
@@ -7,22 +7,23 @@ output_file="output.md"
 > $output_file
 echo "# Current Versions In Each Environment" >> $output_file
 for i in $(find . -type f -name "values.yaml"); do
-    #echo "-------------------------" >> $output_file
-    echo "## $i" >> $output_file
-    directory_name=$(basename "$(dirname "$i")")
-    #echo "## $directory_name" >> $output_file
-    echo "These are the current versions of this environment {**$directory_name**}" >> $output_file
-    #echo "## $i" >> $output_file
-    #echo "-------------------------" >> $output_file
-    image=$(grep -o 'image:\s\+\S\+' "$i" | awk '{print $2}')
-    imageTag=$(grep -o 'imageTag:\s\+\S\+' "$i" | awk '{print $2}')
-   
+    # Extract lines containing "image" and "imageTag"
+    image_lines=$(cat "$i" | grep -E 'image:|imageTag:')
+
     # Create a Markdown table header
     echo "| Image | ImageTag |" >> "$output_file"
-    echo "|-------|----------|" >> "$output_file"
-        
-    # Print the extracted information as a row in the table
-    echo "| $image | $imageTag |" >> "$output_file" 
+    echo "|-------|---------|" >> "$output_file"
+
+    # Use a while loop to process the image and imageTag values
+    while read -r line; do
+        if [[ $line =~ ^image:\ (.+) ]]; then
+            image="${BASH_REMATCH[1]}"
+        elif [[ $line =~ ^imageTag:\ (.+) ]]; then
+            imageTag="${BASH_REMATCH[1]}"
+            # Print the extracted information as a row in the table
+            echo "| $image | $imageTag |" >> "$output_file"
+        fi
+    done <<< "$image_lines"
 done
 if ! git diff --quiet -- "$output_file"; then
     # Add, commit, and push the file to the GitHub repository
